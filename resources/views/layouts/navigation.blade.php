a842de8703c451cb127ccc135a1f1947c0bf2c38
marchenkovby <marchenkovby@gmail.com>
2025-03-07 15:28:39 +0300

    Update logo in app template

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('index') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <x-application-logo class="block h-9 w-auto fill-current text-sky-900 dark:text-sky-100" />
                     </a>
                 </div>
 
