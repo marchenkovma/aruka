aee1af71391036d746e7fc001f04519d31ebe1c5
marchenkovby <marchenkovby@gmail.com>
2025-03-07 15:37:15 +0300

    Update logo in guest template

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <x-application-logo class="block h-9 w-auto fill-current text-sky-900 dark:text-sky-100" />
                 </a>
             </div>
 
