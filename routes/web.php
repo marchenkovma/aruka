c815dd987d77c875ff6282ffcbad158d4ff83d71
marchenkovby <marchenkovby@gmail.com>
2025-03-17 12:38:04 +0300

    refactor: rename welcome to home and reorganize structure

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome2');
+    return view('home.index');
 })->name('home.index');
 
 Route::resource('/articles', ArticleController::class);
