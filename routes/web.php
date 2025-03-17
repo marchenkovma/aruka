c10cd3cd8309e80af830260d445e35af8a1b9fe9
marchenkovby <marchenkovby@gmail.com>
2025-03-17 13:57:12 +0300

    refactor: change controller from resourceful to custom routes

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,14 @@ Route::get('/', function () {
     return view('home.index');
 })->name('home.index');
 
-Route::resource('/articles', ArticleController::class);
+#Route::resource('/articles', ArticleController::class);
+
+Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
+Route::post('articles', [ArticleController::class, 'store'])->name('articles.store');
+Route::get('articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
+Route::get('articles/{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
+Route::put('articles/{article}', [ArticleController::class, 'update'])->name('articles.update');
+Route::delete('articles/{article}', [ArticleController::class, 'destroy'])->name('articles.destroy');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
