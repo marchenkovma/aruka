7247d74e79be6145d7be9fd7e16f90630bd3a881
marchenkovby <marchenkovby@gmail.com>
2025-03-17 17:37:12 +0300

    refactor: update article component UI

--- resources/views/components/article.blade.php
+++ resources/views/components/article.blade.php
@@ -1,5 +1,5 @@
 <article class="py-6">
-    <div class="mb-3 flex items-center justify-between text-gray-500">
+    <div class="mb-3 flex items-center justify-between">
         <div>
             @foreach ($article->tags as $tag)
                 <a
@@ -10,34 +10,46 @@
                 </a>
             @endforeach
         </div>
-        <span class="text-sm">
+        <span class="text-sm text-gray-600 dark:text-gray-400">
             {{ __("Published") }}
             <time datetime="{{ $article->created_at }}">
                 {{ $article->created_at->diffForHumans() }}
             </time>
         </span>
     </div>
-    <h2
-        class="mb-2 text-2xl font-bold tracking-tight text-gray-900 hover:underline dark:text-white"
+    <a
+        href="{{ route("articles.show", $article->id) }}"
+        class="hover:underline"
     >
-        <a href="{{ route("articles.show", $article->id) }}">
+        <h2 class="mb-2 text-2xl font-bold tracking-tight">
             {{ $article->title }}
-        </a>
-    </h2>
-    <p class="mb-5 text-gray-500 dark:text-gray-400">
+        </h2>
+    </a>
+    <!-- Блок превью для статьи -->
+    {{-- @if ($article->preview_image) --}}
+    <div class="mb-4">
+        <img
+            src="{{ $article->preview_image }}"
+            alt="{{ $article->title }}"
+            class="w-full rounded object-cover"
+        />
+    </div>
+    {{-- @endif --}}
+
+    <p class="mb-5 text-gray-700 dark:text-gray-300">
         {{ Str::limit($article->content, 150) }}
     </p>
     <div class="flex items-center justify-between">
         <a
             class="flex items-center space-x-2"
-            href="/blog/author/{{-- $article->author->slug --}}"
+            href="{{-- $article->author->slug --}}"
         >
             <img
                 class="h-7 w-7 rounded-full"
                 src="{{-- $article->author->profile_picture --}}"
                 alt="{{-- $article->author->name --}} profile picture"
             />
-            <span class="font-medium dark:text-white">
+            <span class="font-medium">
                 {{-- $article->author->name --}}
             </span>
         </a>
@@ -45,7 +57,7 @@
             class="inline-flex items-center font-medium text-blue-600 hover:underline dark:text-blue-500"
             href="{{ route("articles.show", $article->id) }}"
         >
-            Read more
+            {{ __("Read more") }}
             <svg
                 class="ml-2 h-4 w-4"
                 fill="currentColor"
